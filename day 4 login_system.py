f470373b1d4dfefa8f8b6ade5e4907417722ce57
HAMMAD <ashrafhammad796@gmail.com>
2026-07-19 18:36:49 +0500

    Day 4: Student Report Card and Login Authentication System

--- day 4 login_system.py
+++ day 4 login_system.py
@@ -1,8 +1,6 @@
 username= input("Enter username:")
 password= input("Enter password:")
-if username == hammad and password ==1111:
+if username =="hammad" and password =="1111":
     print("login successfully")
-elif username != hammad or password != 1111:
+elif username != "hammad" or password != "1111":
     print("invalid username or password")
-else:
-    print("Invalid Username or Password")
